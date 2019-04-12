2b1d4953b3bf1111fd2e4db05143c7941897a8aa
verhulstm <michael@terminallabs.com>
2019-04-11 22:09:30 -0500

    now the lxc containers have salt in them

--- spawn_lxc_group.sh
+++ spawn_lxc_group.sh
@@ -2,3 +2,11 @@ for i in `seq 0 4`;
 do
   lxc launch ubuntu:18.04 c$i  
 done
+
+for i in `seq 0 4`;
+do
+  lxc exec c$i  -- apt update
+  lxc exec c$i  -- apt -y upgrade
+  lxc exec c$i  -- wget -O bootstrap-salt.sh https://bootstrap.saltstack.com
+  lxc exec c$i  -- sudo sh bootstrap-salt.sh -P -X git v2018.3.3
+done
